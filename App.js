a33df80c3bc2e646f8d77cabba596ef6ee8c862c
unknown <aisha.maharjan34@gmail.com>
2022-03-20 09:04:46 +0545

    .

---
+++ App.js
@@ -0,0 +1,78 @@
+
+import './App.css';
+import React,{useState} from 'react'
+function App() {
+ 
+
+    const [counter, setCounter] = useState(0)
+  
+  
+  const handleClick1 = () => {
+    
+    setCounter(counter + 1)
+  }
+  const handleClick2 = () => {
+    setCounter(counter - 1)
+  }
+  const handleClick3 = () => {
+    setCounter(0)
+  }
+  
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '300%',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '-15%',
+    }}>
+      Counter App
+      <div style={{
+        fontSize: '120%',
+        position: 'relative',
+        top: '10vh',
+      }}>
+        {counter}
+      </div>
+      <div className="buttons">
+        <button style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginRight: '5px',
+          backgroundColor: 'green',
+          borderRadius: '8%',
+          color: 'white',
+        }}
+          onClick={handleClick1} >Increment</button>
+        <button style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginLeft: '5px',
+          backgroundColor: 'red',
+          borderRadius: '8%',
+          color: 'white',
+        }}
+          onClick={handleClick2} disabled={counter==0}>Decrement</button>
+          <button style={{
+          fontSize: '60%',
+          position: 'relative',
+          top: '20vh',
+          marginLeft: '5px',
+          backgroundColor: 'yellow',
+          borderRadius: '8%',
+          color: 'white',
+        }}
+          onClick={handleClick3} disabled={counter==0}>Reset</button>
+          
+      </div>
+    </div>
+  )
+}
+  
+export default App
